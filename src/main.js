93ab5cfe38c21963f852a1740326e09dbbac3e13
Christopher Amadra Titus <kris.skyfallgraphix@gmail.com>
2024-02-07 03:54:20 +0300

    Initail commit

--- src/main.js
+++ src/main.js
@@ -1,13 +1,7 @@
-require('dotenv').config(); // Load environment variables from .env file
 const { app, BrowserWindow } = require('electron');
 const { autoUpdater } = require('electron-updater');
 const path = require('path');
 const checkInternetConnected = require('check-internet-connected');
-
-// Configure auto-updater
-autoUpdater.autoDownload = true; // Enable auto-download
-autoUpdater.allowPrerelease = true; // Allow pre-release versions for development updates
-
 
 // Initialize the app
 app.on('ready', async () => {
@@ -20,19 +14,22 @@ app.on('ready', async () => {
     webPreferences: {
       nodeIntegration: true,
     },
-    icon: path.join(__dirname, './res/images/', 'myRadioIcon.png')
+    icon: path.join(__dirname, '../res/images/', 'myRadioIcon.png')
   });
-  splashWindow.loadFile('splash.html');
+  splashWindow.loadFile(path.join(__dirname, 'splash.html'));
   splashWindow.setResizable(false);
   splashWindow.setMenu(null);
 
+
+  // Bring the splash window to the front immediately
+  splashWindow.focus();
   // Check for internet connectivity periodically
   setInterval(async () => {
     try {
       await checkInternetConnected();
     } catch (error) {
       // Load the error page if there's no internet connection
-      mainWindow.loadFile('error.html');
+      mainWindow.loadFile(path.join(__dirname, 'error.html'));
     }
   }, 1000); // Check every second
 
@@ -42,7 +39,7 @@ app.on('ready', async () => {
     height: 600,
     show: false, // Initially hide the window
     // Other window options...
-    icon: path.join(__dirname, './res/images/', 'myRadioIcon.png')
+    icon: path.join(__dirname, '../res/images/', 'myRadioIcon.png')
   });
   mainWindow.setMenu(null);
   mainWindow.setResizable(true); // Allow resizing initially
@@ -62,35 +59,6 @@ app.on('ready', async () => {
       mainWindow.show();
     }, 2000); // 2000 milliseconds = 2 seconds
   });
-
-  // Listen for update events
-  autoUpdater.on('checking-for-update', () => {
-    console.log('Checking for update...');
-  });
-
-  autoUpdater.on('update-available', (info) => {
-    console.log('Update available:', info.version);
-  });
-
-  autoUpdater.on('update-not-available', () => {
-    console.log('Update not available.');
-  });
-
-  autoUpdater.on('error', (error) => {
-    console.error('Update error:', error.message);
-  });
-
-  autoUpdater.on('download-progress', (progressObj) => {
-    console.log('Download progress:', progressObj);
-  });
-
-  autoUpdater.on('update-downloaded', (info) => {
-    console.log('Update downloaded:', info.version);
-    autoUpdater.quitAndInstall();
-  });
-
-  // Trigger the update check
-  autoUpdater.checkForUpdates();
 });
 
 // Close all windows on macOS when the app is closed
